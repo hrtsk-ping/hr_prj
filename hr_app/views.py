7b55a2b7f01bd80fccc7bbb2e6a28cc0d53ec471
hrtsk-ping <t.hrtsk.ping@gmail.com>
2022-03-23 17:31:07 +0900

    FIX: make partial templates

--- hr_app/views.py
+++ hr_app/views.py
@@ -2,3 +2,12 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class AboutView(generic.TemplateView):
+    template_name = "about.html"
+
+class ContactView(generic.TemplateView):
+    template_name = "contact.html"
+
+class PostView(generic.TemplateView):
+    template_name = "post.html"
